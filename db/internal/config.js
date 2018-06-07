1ad5e193dac11aa99b51a2d6d077af05c1464487
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-07 18:56:12 +0300

    path changes

--- db/internal/config.js
+++ db/internal/config.js
@@ -3,7 +3,7 @@ const config = require('nconf');
 config
     .argv()
     .env()
-    .file({ file: '../../hbud.config.json' });
+    .file({ file: '../../../hbud.config.json' });
 
 config.get('env') && (process.env.NODE_ENV = config.get('env'));
 
